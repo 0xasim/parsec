19710be62cf1e04ea9602a742f147fda0bec1ea5
dude <asimalilatif@gmail.com>
2020-09-05 17:14:02 +0500

    added escape in noxss

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from markupsafe import escape
 app = Flask(__name__)
+
 
 @app.route('/')
 def hell_world():
-  return 'Hello, World!'
+  return 'Vulnerable web application'
 
 @app.route('/xss/<value>')
 def xss(value):
-  return value
+  return f'value is: {value}'
+
+@app.route('/noxss/<value>')
+def noXss(value):
+  return f'value is {escape(value)}'
