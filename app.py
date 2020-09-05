532790f643712660197d9495bcd5fabc9ca42bdf
dude <asimalilatif@gmail.com>
2020-09-05 17:25:45 +0500

    added comments

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask
 from markupsafe import escape
 app = Flask(__name__)
 
-
 @app.route('/')
 def hell_world():
   return 'Vulnerable web application'
 
+"""
+Vulnerability occurs because a user controlled input is rendered back without any escaping (sanitization).
+
+Looks like flask doesn't escape variable URLs by default.
+https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+No need to escape  int, float, path, uuid?
+"""
+
 @app.route('/xss/<value>')
 def xss(value):
   return f'value is: {value}'
+
+"""
+Can be prevented by escaping value using escape function in markupsafe module
+"""
 
 @app.route('/noxss/<value>')
 def noXss(value):
